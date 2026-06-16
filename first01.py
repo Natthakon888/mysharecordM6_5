a5dfe52d8bf33e0b0b8e58a8e2d4b6bfdaa58e1f
Natthakon888 <164258@wsk.ac.th>
2026-06-16 04:17:12 +0000

    first01.py

---
+++ first01.py
@@ -0,0 +1,6 @@
+print("โปรแกรมคำนวณหาพื้นที่สามเหลี่ยม")
+h=float(input("enter hight:"))
+b=float(input("enter base:"))
+area=0.5*h*b
+print("พื้นที่สามเหลี่ยมท่ากับ",area)
+print(จัดทำโดย นาย ณัฐกรณ์ สาคร เลขที 9 )
